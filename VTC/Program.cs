91ba7338827486c471425007e134d768b3b86e08
VladC-ux <64519475+VladC-ux@users.noreply.github.com>
2023-06-18 11:46:45 +0400

    Add project files.

---
+++ VTC/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Globalization;
+using System.Net.Http.Headers;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Security.AccessControl;
+using System.Xml;
+
+namespace VTC
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            User user = new User();
+
+            Console.WriteLine("Введите новый пароль:");
+            string newPassword = Console.ReadLine();
+            
+            try
+            {
+                user.ChangePassword(newPassword);
+                Console.WriteLine("Пароль успешно изменен.");
+            }
+            catch (Exception ex) 
+            {
+                Console.WriteLine("Ошибка при изменении пароля,нужны как минимум две цифры : " + ex.Message);
+            }
+
+           
+        }
+   }
+}
+
+
---
+++ VTC/User.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VTC
+{
+    internal class User:Person
+    {
+       
+      
+        public void ChangePassword(string newPassword)
+        {
+            bool isRightPassword = false;
+            int digitCount = 0;
+
+            do
+            {
+                if (newPassword == null || newPassword.Length < 8)
+                {
+                    Console.WriteLine("Пароль должен содержать минимум 2 цифры и состаяать из 8 букв и цифр. Попробуйте еще раз.");
+                    newPassword = Console.ReadLine();
+                }
+
+                digitCount = 0;
+
+                foreach (var item in newPassword)
+                {
+                    if (int.TryParse(item.ToString(), out int b))
+                    {
+                        digitCount++;
+                        if (digitCount >= 2)
+                        {
+                            isRightPassword = true;
+                            break;
+                        }
+                    }
+                }
+
+                if (!isRightPassword)
+                {
+                    Console.WriteLine("Пароль должен содержать минимум 2 цифры. Попробуйте еще раз.");
+                    newPassword = Console.ReadLine();
+                }
+
+            } while (!isRightPassword);
+
+            password = newPassword;
+        }
+
+    }
+}
